a557635e33d3c3a498f03adc9908939f03250494
iweb1997 <iweb.1997@gmail.com>
2018-08-26 12:11:47 +0530

    first commit for angular project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Rajat',mother:'Meena',father:'Subhash' },
+  { id: 2, name: 'Kunal',mother:'Meena',father:'Subhash' },
+  { id: 3, name: 'Sidhi',mother:'Meena',father:'Subhash' },
+  { id: 4, name: 'Bhavya',mother:'Kavita',father:'Parveen' },
+  { id: 5, name: 'Sejal',mother:'Poonam',father:'Soarbh' },
+  { id: 6, name: 'Sidharav',mother:'Kirti',father:'Ashu'}
+];
